928a6e5f3b5b0cb8c61e85f4010f5aacfad06b7f
Navpreet Ahuja <navpreet.ahuja@hotmail.com>
2022-10-27 23:48:31 +1030

    serve multiple apps

--- server/server.js
+++ server/server.js
@@ -3,7 +3,21 @@ const path = require('path');
 const app = express();
 
 const PORT = process.env.PORT || 9000;
+const customerBuildPath = path.join(__dirname, '../customer/build')
+const adminBuildPath = path.join(__dirname, '../admin/build')
 
+// serve static content of customer
+app.use(express.static(customerBuildPath))
+
+app.get('/*', function(req,res) {
+  res.sendFile(customerBuildPath)
+})
+
+app.use('/admin', express.static(adminBuildPath))
+
+app.get('/admin/*', function(req,res) {
+  res.sendFile(adminBuildPath)
+})
 
 app.listen(PORT, () => {
   console.log(`server running on port ${PORT}!`);
